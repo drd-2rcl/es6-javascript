6b43273f4c2094e75301ac9154ad0f48fb8d5774
drd-2rcl <drd.rclrd@gmail.com>
2019-03-31 20:34:23 -0300

    destructuring

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-const soma = (a = 3, b = 6) =>  a + b;
+// destructuring
+const usuario = {
+  nome: 'Diogo',
+  idade: '26',
+  endereco: {
+    cidade: 'São Paulo',
+    estado: 'SP',
+  },
+};
 
-console.log(soma(1));
-console.log(soma());
+function mostraNome({ nome, idade }) {
+  console.log(nome, idade);
+}
+
+mostraNome(usuario);
+
+// const { nome, idade, endereco: { cidade } } = usuario;
+
+// console.log(nome);
+// console.log(idade);
+// console.log(cidade);
