8a172c6ca539240ae9c891e2a45c5919c82be106
drd-2rcl <drd.rclrd@gmail.com>
2019-03-31 20:17:02 -0300

    arrow functions

--- bundle.js
+++ bundle.js
@@ -1,25 +1,16 @@
 "use strict";
 
-//operações em arrays
-var arr = [1, 3, 4, 5, 8, 9, 10]; // map = percorre o vetor e retorna algo de dentro
-
+// arrow functions
+var arr = [1, 3, 4, 5, 6];
 var newArr = arr.map(function (item) {
   return item * 2;
 });
-console.log(newArr); // reduce = consome todo o vetor e transforma em uma única variavel
+console.log(newArr);
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum); // filter = filtra '¬¬ lol, sempre retorna true or false
+var teste = function teste() {
+  return {
+    nome: 'Diogo'
+  };
+};
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0; // par
-});
-console.log(filter); // find = é utilizado para verificar se existe alguma info dentro do array ou se consegue encontrar a info dentro do arr. Sempre retorna true or false.
-
-var find = arr.find(function (item) {
-  // return item === 4;
-  return item === 2;
-});
-console.log(find);
+console.log(teste());
--- main.js
+++ main.js
@@ -1,36 +1,11 @@
-//operações em arrays
+// arrow functions
 
-const arr = [1, 3, 4, 5, 8, 9, 10];
+const arr = [1, 3, 4, 5, 6];
 
-// map = percorre o vetor e retorna algo de dentro
-const newArr = arr.map(function(item) {
-  return item * 2;
-})
+const newArr = arr.map(item => item * 2);
 
 console.log(newArr);
 
-// reduce = consome todo o vetor e transforma em uma única variavel
-const sum = arr.reduce(function(total, next) {
-  return total + next;
-});
+const teste = () => ({ nome: 'Diogo' });
 
-console.log(sum);
-
-
-// filter = filtra '¬¬ lol, sempre retorna true or false
-const filter = arr.filter(function(item){
-  return item % 2 === 0; // par
-})
-
-console.log(filter);
-
-
-// find = é utilizado para verificar se existe alguma info dentro do array ou se consegue encontrar a info dentro do arr. Sempre retorna true or false.
-
-const find = arr.find(function(item) {
-  // return item === 4;
-  return item === 2;
-});
-
-console.log(find);
-
+console.log(teste());
