d8ac87dadfd77aaf2f1e72fe625f94d1968f2192
drd-2rcl <drd.rclrd@gmail.com>
2019-03-31 22:03:29 -0300

    template literals

--- main.js
+++ main.js
@@ -1,61 +1,8 @@
-// operadores rest e spread
+// TEMPLATE LITERALS
 
-// REST = resto das propriedades
+const nome = 'Diogo';
+const idade = 26;
 
-// EXEMPLO 1
-// const usuario = {
-//   nome: 'Diogo',
-//   idade: 26,
-//   empresa: 'Nenhuma'
-// }
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`); 
 
-// const { nome, ...resto } = usuario;
-
-// console.log(nome);
-// console.log(resto);
-
-// EXEMPLO 2
-// const arr = [1,2,3,4];
-// const [a,b, ...c] = arr;
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-// EXEMPLO 3
-
-// function soma(...params){
-//   return params.reduce((total, next) => total + next);
-// }
-
-// console.log(soma(1, 3, 4));
-
-
-// EXEMPLO 4
-// function soma(a, b, ...params){
-//   return params;
-// }
-// console.log(soma(1, 3, 4, 4, 5, 7));
-
-// --------//--------
-
-// SPREAD = repassa as informações de um objeto ou array para outra estrutura de dados
-
-// EXEMPLO 1
-// const arr1 = [1,2,3];
-// const arr2 = [4,5,6];
-
-// const arr3 = [...arr1, ...arr2];
-
-// console.log(arr3);
-
-// EXEMPLO 2
-const usuario1 = {
-  nome: 'Diogo',
-  idade: '26',
-  empresa: 'nenhuma',
-}
-
-const usuario2 = { ...usuario1, nome: 'Vanderlei' };
-
-console.log(usuario2);
+// console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.'); 
