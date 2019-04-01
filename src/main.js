47a644b00b9ef79ac568449d0d21766bb2c961f3
drd-2rcl <drd.rclrd@gmail.com>
2019-04-01 20:27:39 -0300

    working with promises async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-alert('cacildis');
+// ASYNC AWAIT ES8
+
+// promise para simular algo que vai demorar 2 segundos para executar;
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => { resolve('OK') }, 2000);
+});
+
+//resposta: > es8
+// async e await com a function normal
+// async function executaPromise() {
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+// }
+// executaPromise();
+
+// async e await com arrow function
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+executaPromise();
+
+// resposta: < es8
+// minhaPromise()
+//   .then(response => {
+//   console.log(response);
+//   })
+//   .catch(err => {
+
+//   })
