d2f206c035fc272080ae2bc6690a8e932e4c1a77
drd-2rcl <drd.rclrd@gmail.com>
2019-04-01 19:49:45 -0300

    possibilidades de import e export

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-// CONFIG WEBPACK
+// import { soma as somaFunction, sub } from './funcoes';
+// import somaFunction from './soma';
+// import soma, { sub} from './funcoes';
+import * as funcoes from './funcoes';
 
-import { soma } from './funcoes';
+console.log(funcoes);
+console.log(funcoes.soma(1,2));
+console.log(funcoes.sub(4,2));
 
-console.log(soma(1,2));
+
+// console.log(somaFunction(1,2));
+// console.log(soma(1,2));
+// console.log(sub(4,2));
