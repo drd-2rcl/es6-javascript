e0076574588ea923cd4b7945cac040fb093ea4c7
drd-2rcl <drd.rclrd@gmail.com>
2019-04-01 21:39:53 -0300

    add and render elements with vanilla js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
   }
@@ -20,7 +21,34 @@ class App {
       html_url: 'https://github.com/drd-2rcl',
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+
+  render () {
+    this.listEl.innerHTML = "";
+    // diferença entre o map e forEach. O map faz alterações no array, o forEach só percorre.
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    }) 
   }
 }
 
