20d457f39c5221441e366192426784476641bb0b
drd-2rcl <drd.rclrd@gmail.com>
2019-04-04 19:58:48 -0300

    Loading and error

--- src/main.js
+++ src/main.js
@@ -15,6 +15,19 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if(loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando...'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -23,21 +36,31 @@ class App {
     if (repoInput.length === 0)
       return;
 
-    const response = await api.get(`/repos/${repoInput}`);
-    console.log(response);
+    this.setLoading();
 
-    const { name, description, html_url, owner: { avatar_url } } = response.data;
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+      console.log(response);
+  
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
+  
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
+  
+      this.inputEl.value = '';
+  
+      this.render();
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    });
+    } catch (err) {
+      alert('O repositório não existe!');
+    }
 
-    this.inputEl.value = '';
+    this.setLoading(false);
 
-    this.render();
   }
 
   render () {
